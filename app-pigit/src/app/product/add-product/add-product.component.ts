b8df38dac2ddeafc423a95437465009cbe424502
NONAE\goes__000 <ranate_sirinoy@cmu.ac.th>
2018-07-20 15:50:04 +0700

    make ui Add product

--- app-pigit/src/app/product/add-product/add-product.component.ts
+++ app-pigit/src/app/product/add-product/add-product.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-add-product',
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-product.component.css']
 })
 export class AddProductComponent implements OnInit {
+  picture = "";
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
+  }
+
+  onSelectFile(event) { // called each time file input changes
+    if (event.target.files && event.target.files[0]) {
+      var reader = new FileReader();
+
+      reader.onload = (event: any) => {
+        this.picture = event.target.result;
+      }
+
+      reader.readAsDataURL(event.target.files[0]);
+    }
   }
 
 }
